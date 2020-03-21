3a7f6f37bd4419eb44220e510fda616859af6728
MisterQuestions <misterquestions.mx@gmail.com>
2020-03-21 15:42:42 -0600

    Added dotenv and challenges endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as health from './health';
+import * as challenges from './challenges';
 
 export class Routes {
   private readonly router: Router = Router();
@@ -10,6 +11,8 @@ export class Routes {
 
   public initialize(): void {
     this.router.get('/health', health.index);
+
+    this.router.get('/challenges', challenges.index);
   }
 
   public getRouter(): Router {
